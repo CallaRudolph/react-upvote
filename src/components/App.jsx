b22d4d156e82ecb04126c384b4210ee9d91e2bac
Calla Rudolph and Nathan Stewart <nathan@>
2017-09-19 09:54:46 -0700

    create MasterPost parent component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import MasterPost from "./MasterPost";
 
 function App(props){
   return (
     <div>
-      <Header/>  
+      <Header/>
+      <MasterPost/>
     </div>
   );
 }
---
+++ src/components/MasterPost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MasterPost() {
+  return(
+    <div>
+      <h2>The landing page of fun.</h2>
+    </div>
+  );
+}
+
+export default MasterPost;
