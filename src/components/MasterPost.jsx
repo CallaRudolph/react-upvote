ebb78257b10667eb9e3d09c65915e6a4c81ec614
Calla Rudolph and Nathan Stewart <calla@>
2017-09-19 09:56:42 -0700

    create NewPostForm component

--- src/components/MasterPost.jsx
+++ src/components/MasterPost.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import NewPostForm from "./NewPostForm";
 
 function MasterPost() {
   return(
     <div>
       <h2>The landing page of fun.</h2>
+      <NewPostForm/>
     </div>
   );
 }
---
+++ src/components/NewPostForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewPostForm(){
+  return(
+    <div>
+      <h3>This will soon be a form.</h3>
+    </div>
+  );
+}
+
+export default NewPostForm;
