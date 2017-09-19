65be9dcec9b43bbbbc71139377ba7b588c7d5aee
Calla Rudolph and Nathan Stewart <nathan@>
2017-09-19 10:15:39 -0700

    create new post form with action and event handler

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
+import c from "./../constants";
+import { v4 } from "uuid";
 
-function NewPostForm(){
-  return(
-    <div>
-      <h3>This will soon be a form.</h3>
-    </div>
-  );
+class NewPostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewPost = this.handleNewPost.bind(this);
+  }
+
+  handleNewPost(event) {
+    event.preventDefault();
+    const { _title, _content } = this.refs;
+    const { dispatch } = this.props;
+    const action = {
+      type: c.ADD_POST,
+      id: v4(),
+      title: _title.value,
+      content: _content.value,
+      timeStamp: new Date().getTime(),
+      upvotes: null,
+      downvotes: null
+    };
+    dispatch(action);
+  }
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleNewPost}>
+          <input
+            ref="_title"
+            type="text"
+            id="title"
+            placeholder="Title"/>
+          <br/>
+          <textarea
+            ref="_content"
+            id="content"
+            placeholder="What is this about"/>
+          <br/>
+          <button type="submit">Go for it</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+NewPostForm = connect()(NewPostForm);
 
 export default NewPostForm;
